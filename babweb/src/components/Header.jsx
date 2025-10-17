d6132e09c70d4edcd1aeeb0a1e3d24784b928b26
yves40 <yves.toubhans@free.fr>
2025-10-17 18:24:40 +0200

    Version and message

--- babweb/src/components/Header.jsx
+++ babweb/src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="header">
       <div className="header__div--left">
-        <h3 className=" font-bold">Mise à jour</h3>
+        <h3 className=" font-bold">Les news</h3>
         <p>Rien à signaler depuis le début du mois.</p>
         <p>Les derniers livres achetés n'ont pas encore été rentrés dans la base.</p>
       </div>
