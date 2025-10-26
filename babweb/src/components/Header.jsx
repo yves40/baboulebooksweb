4cd2636d5e0afb60d99a72b21acb77e3797d59cc
yves40 <yves.toubhans@free.fr>
2025-10-26 10:40:31 +0100

    css class names

--- babweb/src/components/Header.jsx
+++ babweb/src/components/Header.jsx
@@ -25,8 +25,8 @@ function Header() {
       // console.log('Header mount');
       
   return (
-    <header className="header">
-      <div className="header__div--left">
+    <header className="news">
+      <div className="news__div--left">
         <h3 className=" font-bold">Les news</h3>
         <p>Rien à signaler depuis le début du mois.</p>
         <p>Les derniers livres achetés n'ont pas encore été rentrés dans la base.</p>
