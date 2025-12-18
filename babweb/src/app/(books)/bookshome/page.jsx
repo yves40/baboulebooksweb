c1f85799a2c4fb993787fae5f29b7e906e659a00
yves40 <yves.toubhans@free.fr>
2025-12-18 12:37:09 +0100

    Start working on searches

--- babweb/src/app/(books)/bookshome/page.jsx
+++ babweb/src/app/(books)/bookshome/page.jsx
@@ -4,6 +4,7 @@ import { useEffect, useRef, useState } from 'react'
 import { getBooksCount } from '@/server/books/books';
 import { getAuthorsCount } from '@/server/books/authors';
 import { getEditorsCount } from '@/server/books/editors';
+import InputText from '@/components/InputText';
 
 export default function page() {
 
@@ -11,7 +12,13 @@ export default function page() {
   const [bookscount, setBookscount] = useState(0);
   const [authorscount, setAuthorscount] = useState(0);
   const [editorscount, setEditorscount] = useState(0);
+  const [booktitle, setBooktitle] = useState('');
+  const [bookauthor, setBookauthor] = useState('');
+  const [bookeditor, setBookeditor] = useState('');
   const stats = useRef('stats');
+  const bookselector = useRef('bookselector');
+  const authorselector = useRef('authorselector');
+  const editorselector = useRef('editorselector');
 
   // -----------------------------------------------------------------------------
   // Get books count from server
@@ -60,6 +67,19 @@ export default function page() {
     stats.current.hidden = false;
   }, []);
 
+  // -----------------------------------------------------------------------------
+  // Track search criterias updates
+  // -----------------------------------------------------------------------------
+  useEffect( () => {
+    console.log(`Title search update, now find: =================== ${booktitle}`);
+  }, [booktitle]);
+  useEffect( () => {
+    console.log(`Author search update, now find: =================== ${bookauthor}`);
+  }, [bookauthor]);
+  useEffect( () => {
+    console.log(`Editor search update, now find: =================== ${bookeditor}`);
+  }, [bookeditor]);
+
 
   return (
     <div className="page__container">
@@ -69,20 +89,24 @@ export default function page() {
         <p>Faire des recherches par titre, auteur, éditeur</p>
         <p className=' my-3 underline'>Quelques statistiques sur vos livres dans la base :</p>
         <ul className=' max-w-1/2 m-6' hidden ref={stats}>
-          <li className='flex justify-between'>
-            <span>Nombre total de livres</span>
-            <span className='text-red-700'>{bookscount}</span>
-            </li>
-          <li  className='flex justify-between'>
-            <span>Nombre d'auteurs</span>
-            <span className='text-red-700'>{authorscount}</span>
-             
+          <li className='flex justify-end'>
+            <span className=' mx-4'>Nombre total de livres</span>
+            <span className='text-red-700 mx-4'>{bookscount}</span>
+            <span ref={bookselector}><img className='svgsmall-blue' src="svg/magnifying-glass-solid.svg" alt="" /></span>
           </li>
-          <li  className='flex justify-between'>
-            <span>Nombre d'éditeurs</span>
-            <span className='text-red-700'>{editorscount}</span>
-             
+          <InputText className=' mx-4' componentid="booktitle" label="Rechercher un titre"  parentHandler={setBooktitle} timeout={2000}> </InputText>
+          <li  className='flex justify-end'>
+            <span className=' mx-4'>Nombre d'auteurs</span>
+            <span className='text-red-700 mx-4'>{authorscount}</span>
+            <span ref={authorselector}><img className='svgsmall-blue' src="svg/magnifying-glass-solid.svg" alt="" /></span>             
           </li>
+          <InputText componentid="booktitle" label="Rechercher un auteur"  parentHandler={setBookauthor} timeout={2000}> </InputText>
+          <li  className='flex justify-end'>
+            <span className=' mx-4'>Nombre d'éditeurs</span>
+            <span className='text-red-700 mx-4'>{editorscount}</span>
+            <span ref={editorselector}><img className='svgsmall-blue' src="svg/magnifying-glass-solid.svg" alt="" /></span>
+          </li>
+          <InputText componentid="booktitle" label="Rechercher un éditeur"  parentHandler={setBookeditor} timeout={2000}> </InputText>
         </ul>
       </div>
     </div>
