4d297c63de3330b2f3c9b576584ccc61741260d7
yves40 <yves.toubhans@free.fr>
2025-12-28 18:40:56 +0100

    Remove logs

--- babweb/src/redux/menuProperties.js
+++ babweb/src/redux/menuProperties.js
@@ -7,27 +7,24 @@ const initialState =
         useremail : "",
         menustatus: false   // used to manage menu pop up on phones
     }
-
-const logger = new Logger();
-const menuSlice = createSlice(
-    {
+    
+    const logger = new Logger();
+    const menuSlice = createSlice(
+        {
         name: "menuProperties",
         initialState,
         reducers: 
         {
-            loginUser: (state, action) => {       
-              state.logged = true;
-              state.useremail = action.payload.useremail;    
-              logger.info('Redux slice : User login');              
+            loginUser: (state, action) => {     
+                state.logged = true;
+                state.useremail = action.payload.useremail;    
             },
             disconnectUser: (state, action) => {           
-              state.logged = false;
-              state.useremail = "";
-              logger.info('Redux slice : User logout');              
+                state.logged = false;
+                state.useremail = "";
             },
             toggleMenuStatus: (state, action) => {
                 state.menustatus = state.menuvisible;
-                logger.info(`Redux slice : ToggleMenu status ${action.payload.menuvisible}`);              
             }
         }
     }
